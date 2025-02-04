823931253c319f9e928c41004b5a329c0c3740d8
J0SHUA-18 <deguzmanjoshua891@gmail.com>
2025-02-04 08:35:02 +0800

    Add files via upload

---
+++ DEGUZMAN_Activity08.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+//addition
+int add(int num1, int num2);
+double add(double num1, double num2);
+
+//subtruction
+int subtract(int num1, int num2);
+double subtract(double num1, double num2);
+
+//multiplication
+int multiply(int num1, int num2);
+double multiply (double num1, double num2);
+
+int main(){
+	//additon
+	cout << add(3,3) << endl;
+	cout << add(7.53,2.69)<< endl;
+	//subtruction
+	cout << subtract(3,9) << endl;
+	cout << subtract(7.53,2.69)<< endl;
+	//multiplication
+	cout << multiply(3,9) << endl;
+	cout << multiply(7.53,2.69)<< endl;
+	//multiplication
+	cout << devide(3,9) << endl;
+	cout << devide(7.53,2.69)<< endl;
+	
+	return 0;
+}
+
+	//additon
+int add(int num1, int num2){
+	return num1 + num2;
+}
+	//subtruction
+	int subtract(int num1, int num2){
+	return num1 - num2;
+}
+	//multiplication
+	int multiply(int num1, int num2){
+	return num1 * num2;
+}
+	//additon
+double add(double num1, double num2){
+	return num1 + num2;
+}
+	//subtruction
+double subtract(double num1, double num2){
+	return num1 - num2;
+}
+//multiplication
+double multiply(double num1, double num2){
+	return num1 * num2;
+}
+
